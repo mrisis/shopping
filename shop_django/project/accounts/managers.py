b20579303dc7b224ed388e96ac5a6e403545856c
mrisis <rezaamin8889@gmail.com>
2022-11-30 17:08:03 +0330

    managers.py | create manager | rezaamin

---
+++ shop_django/project/accounts/managers.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+
+class UserManager(BaseUserManager):
+    def create_user(self,phone_number,email,full_name,password):
+        if not phone_number:
+            raise ValueError('user must have be phone number')
+        if not email :
+            raise ValueError('user must have be email')
+        if not full_name:
+            raise ValueError('user must have be full name')
+
+        user = self.model(phone_number=phone_number,email=self.normalize_email(email) , full_name=full_name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+
+    def create_superuser(self,phone_number,email,full_name,password):
+        user = self.create_user(phone_number,email,full_name,password)
+        user.is_admin=True
+        user.save(using=self._db)
+        return user
+
+
+
+
+
+
+
+
+
+
+
+
+
