703271307a000f61d65adba20399fdc0b94bdf92
mrisis <rezaamin8889@gmail.com>
2022-12-02 10:02:45 +0330

    views.py | create UserRegisterView  | rezaamin

--- shop_django/project/accounts/views.py
+++ shop_django/project/accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
+from . forms import RegisterationForm
 
-# Create your views here.
+
+class UserRegisterView(View):
+    form_class = RegisterationForm
+    def get(self,request):
+        form = self.form_class
+        return render(request,'home/register.html',{'form':form})
+
+    def post(self,request):
+        pass
+
+
+
