0da31056fb50e3b59da29990f13b15d7194b67f1
mrisis <rezaamin8889@gmail.com>
2022-12-03 10:42:54 +0330

    settings.py | add media files root and url | rezaamin

--- shop_django/project/config/settings.py
+++ shop_django/project/config/settings.py
@@ -122,6 +122,10 @@ STATICFILES_DIRS= [
     BASE_DIR/'static'
 ]
 
+#media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT= BASE_DIR/'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
