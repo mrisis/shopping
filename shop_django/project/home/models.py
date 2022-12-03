af25ceed8a215680a4211a6b5703497f49f9cff4
mrisis <rezaamin8889@gmail.com>
2022-12-03 10:41:55 +0330

    models.py | create Product , Category model | rezaamin

--- shop_django/project/home/models.py
+++ shop_django/project/home/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100 , unique=True)
+
+    class Meta:
+        ordering = ('name' , )
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category , on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100,unique=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d')
+    description = models.TextField()
+    price = models.IntegerField()
+    availabel = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
