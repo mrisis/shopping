cbb750541c852bfe9142b2d9b38edda9c49db187
mrisis <rezaamin8889@gmail.com>
2022-12-03 16:34:43 +0330

    bucket.py | config bucket avran cloud | rezaamin

---
+++ shop_django/project/bucket.py
@@ -0,0 +1,17 @@
+import boto3
+from django.conf import settings
+
+class Bucket:
+    '''CDN Bucket manager
+    init method creates manager
+
+    '''
+
+    def __init__(self):
+        session = boto3.session.Session()
+        self.connection = session.client(
+            service_name = settings.AWS_SERVICE_NAME,
+            aws_access_key = settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY,
+            endpoint_url = settings.AWS_S3_ENDPOINT_URL
+        )
