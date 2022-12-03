93aec1c2f6d3975389f49dba571e32f4c0fc5d0d
mrisis <rezaamin8889@gmail.com>
2022-12-03 17:06:14 +0330

    bucket.py | add fuction for show bucket | rezaamin

--- shop_django/project/bucket.py
+++ shop_django/project/bucket.py
@@ -5,13 +5,37 @@ class Bucket:
     '''CDN Bucket manager
     init method creates manager
 
+    NOTE:
+        none of this method are aync. use public in tasks.py modules instead.
+
     '''
 
     def __init__(self):
         session = boto3.session.Session()
         self.connection = session.client(
             service_name = settings.AWS_SERVICE_NAME,
-            aws_access_key = settings.AWS_ACCESS_KEY_ID,
+            aws_access_key_id = settings.AWS_ACCESS_KEY_ID,
             aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY,
             endpoint_url = settings.AWS_S3_ENDPOINT_URL
         )
+
+    def get_objects(self):
+        result = self.connection.list_objects_v2(Bucket=settings.AWS_STORAGE_BUCKET_NAME)
+        return result
+
+
+bucket = Bucket()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
