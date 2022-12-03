15f065dc2e9bef33608607b2355722f31af65588
mrisis <rezaamin8889@gmail.com>
2022-12-03 14:57:41 +0330

    views.py | create MenuView | rezaamin

--- shop_django/project/home/views.py
+++ shop_django/project/home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from . models import Product
 class HomeView(View):
     def get(self,request):
         return render(request,'home/home.html')
+
+
+class MenuView(View):
+    def get(self,request):
+        products = Product.objects.filter(availabel=True)
+        return render(request,'home/menu.html',{'products':products})
