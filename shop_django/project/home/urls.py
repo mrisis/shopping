54dfceec4dc3af10e0d90adf19957073d7caf26d
mrisis <rezaamin8889@gmail.com>
2022-12-03 17:06:59 +0330

    urls.py | add url for boucket | rezaamin

--- shop_django/project/home/urls.py
+++ shop_django/project/home/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name='home'
 urlpatterns=[
+    path('bucket/' , views.BucketHome.as_view() , name='bucket'),
     path('',views.HomeView.as_view(),name='home'),
     path('menu/',views.MenuView.as_view(), name='menu'),
     path('<slug:slug>/', views.ProductDetailView.as_view() , name='product_detail'),
