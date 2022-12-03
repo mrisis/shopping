abb888d15fb0c6f1e36504c2bc8b8762f62d5383
mrisis <rezaamin8889@gmail.com>
2022-12-03 15:52:21 +0330

    models.py | fix bug | rezaamin

--- shop_django/project/home/models.py
+++ shop_django/project/home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -29,6 +30,10 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('home:product_detail',args=[self.slug])
+
 
 
 
