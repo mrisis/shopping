717f50f80af3e9b80f0fe6a89fe9ef16b072737d
mrisis <rezaamin8889@gmail.com>
2022-12-04 15:04:35 +0330

    admin.py | add product row id field | rezaamin

--- shop_django/project/home/admin.py
+++ shop_django/project/home/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from . models import Category,Product
 
 admin.site.register(Category)
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    row_id_fields=('category',)
+
 
 
