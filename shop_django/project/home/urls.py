14d23e0957cc767e65c2292762337e2c681548bf
mrisis <rezaamin8889@gmail.com>
2022-12-04 10:11:54 +0330

    urls.py | add url delete object from bucket | rezaamin

--- shop_django/project/home/urls.py
+++ shop_django/project/home/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name='home'
 urlpatterns=[
     path('bucket/' , views.BucketHome.as_view() , name='bucket'),
+    path('delete/obj/bucket/<str:key>/' , views.DeleteBucketObject.as_view() , name='delete_obj_bucket'),
     path('',views.HomeView.as_view(),name='home'),
     path('menu/',views.MenuView.as_view(), name='menu'),
     path('<slug:slug>/', views.ProductDetailView.as_view() , name='product_detail'),
