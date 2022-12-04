84024d30143278ccd67463a8f62db2a4f04761bb
mrisis <rezaamin8889@gmail.com>
2022-12-04 15:57:48 +0330

    urls.py | add url for add product to cart | rezaamin

--- shop_django/project/orders/urls.py
+++ shop_django/project/orders/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name='orders'
 urlpatterns=[
     path('cart/' , views.CartView.as_view() , name='cart'),
+    path('card/add/<int:product_id>/' , views.CartAddView.as_view() , name='cart_add')
 
 ]
