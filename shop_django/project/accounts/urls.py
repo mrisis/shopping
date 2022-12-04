ba919cb82a7c4a972b2c0b3f095b71f1f0525899
mrisis <rezaamin8889@gmail.com>
2022-12-04 10:54:20 +0330

    urls.py | add url for login and logout | rezaamin

--- shop_django/project/accounts/urls.py
+++ shop_django/project/accounts/urls.py
@@ -5,5 +5,7 @@ app_name='accounts'
 urlpatterns=[
     path('register/' , views.UserRegisterView.as_view() , name='user_register'),
     path('register/verify/' , views.UserRegisterVerifyView.as_view() , name='verify_code'),
+    path('logout/' , views.UserLogout.as_view() , name='user_logout'),
+    path('login/' , views.UserLoginView.as_view() , name='user_login'),
 
 ]
