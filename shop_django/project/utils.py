6118d0a677a2f38cce849845ad4ff32b644e0251
mrisis <rezaamin8889@gmail.com>
2022-12-04 11:06:39 +0330

    utils.py | create a mixin for bucket  | rezaamin

--- shop_django/project/utils.py
+++ shop_django/project/utils.py
@@ -1,5 +1,5 @@
 from kavenegar import *
-
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 def send_sms_otp_code(phone_number,code):
     try:
@@ -17,3 +17,6 @@ def send_sms_otp_code(phone_number,code):
         print(e)
 
 
+class IsAdminUserMixin(UserPassesTestMixin):
+    def test_func(self):
+	    return self.request.user.is_authenticated and self.request.user.is_admin
