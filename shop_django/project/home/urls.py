7e3c3a043a968a1be6c09fedc12148915cc774b8
mrisis <rezaamin8889@gmail.com>
2022-12-04 10:27:59 +0330

    urls.py | add url for download bucket object | rezaamin

--- shop_django/project/home/urls.py
+++ shop_django/project/home/urls.py
@@ -6,6 +6,7 @@ app_name='home'
 urlpatterns=[
     path('bucket/' , views.BucketHome.as_view() , name='bucket'),
     path('delete/obj/bucket/<str:key>/' , views.DeleteBucketObject.as_view() , name='delete_obj_bucket'),
+    path('download/obj/bucket/<str:key>/' , views.DownloadBucketObject.as_view() , name='download_obj_bucket'),
     path('',views.HomeView.as_view(),name='home'),
     path('menu/',views.MenuView.as_view(), name='menu'),
     path('<slug:slug>/', views.ProductDetailView.as_view() , name='product_detail'),
