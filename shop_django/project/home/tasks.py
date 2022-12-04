7e3b90e6bf77ba22c9ab46fcff33f9f73d41d9ae
mrisis <rezaamin8889@gmail.com>
2022-12-04 10:11:34 +0330

    tasks.py | add task for delete object | rezaamin

--- shop_django/project/home/tasks.py
+++ shop_django/project/home/tasks.py
@@ -1,4 +1,5 @@
 from bucket import bucket
+from celery import shared_task
 
 def all_bucket_objects_task():
     result = bucket.get_objects()
@@ -6,3 +7,7 @@ def all_bucket_objects_task():
         return result['Contents']
     else:
         return None
+
+@shared_task
+def delete_object_task(key):
+    bucket.delete_object(key)
