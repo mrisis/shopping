497c7a5ab76addee87def0afb469a296ebea582d
mrisis <rezaamin8889@gmail.com>
2022-12-04 15:06:14 +0330

    urls.py | add url for nestes categroy | rezaamin

--- shop_django/project/home/urls.py
+++ shop_django/project/home/urls.py
@@ -10,4 +10,6 @@ urlpatterns=[
     path('',views.HomeView.as_view(),name='home'),
     path('menu/',views.MenuView.as_view(), name='menu'),
     path('<slug:slug>/', views.ProductDetailView.as_view() , name='product_detail'),
+    path('category/<slug:slug_cateory>/' , views.MenuView.as_view(),name='category_filter'),
+
 ]
