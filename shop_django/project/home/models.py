fa808e704749c3d5c5a97183f7b1ef91e9316c5e
mrisis <rezaamin8889@gmail.com>
2022-12-04 15:05:48 +0330

    models.py | changing product and category models | rezaamin

--- shop_django/project/home/models.py
+++ shop_django/project/home/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.urls import reverse
 
 class Category(models.Model):
+    sub_category= models.ForeignKey('self',on_delete=models.CASCADE , related_name='scategory',null=True)
+    is_sub = models.BooleanField(default=False)
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100 , unique=True)
 
@@ -12,9 +14,13 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absloute_url(self):
+        return reverse('home:category_filter',args=[self.slug])
+
+
 
 class Product(models.Model):
-    category = models.ForeignKey(Category , on_delete=models.CASCADE, related_name='products')
+    category = models.ManyToManyField(Category , related_name='products')
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100,unique=True)
     image = models.ImageField()
