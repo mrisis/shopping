ee9358ee703039c5d0cbd1258077484f984cba5d
mrisis <rezaamin8889@gmail.com>
2022-12-05 14:59:39 +0330

    forms.py | create form couponapplyform | rezaamin

--- shop_django/project/orders/forms.py
+++ shop_django/project/orders/forms.py
@@ -3,3 +3,7 @@ from django import forms
 class CardAddForm(forms.Form):
     quantity=forms.IntegerField(label='',min_value=1 , max_value=10,
                                 widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Quantity'}))
+
+
+class CouponApplyForm(forms.Form):
+    code = forms.CharField()
