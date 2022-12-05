a55ace5137f3f0b59334af48c86563aec0417578
mrisis <rezaamin8889@gmail.com>
2022-12-05 14:59:18 +0330

    admin.py | add coupon model in admin panel | rezaamin

--- shop_django/project/orders/admin.py
+++ shop_django/project/orders/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import OrderItem , Order
+from . models import OrderItem , Order , Coupon
 
 
 class OrderItemInLine(admin.TabularInline):
@@ -13,3 +13,4 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderItemInLine,)
 
 
+admin.site.register(Coupon)
