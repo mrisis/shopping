bb9501c6bfbd1c6083ae5f7d0f3146d5b6d60e84
mrisis <rezaamin8889@gmail.com>
2022-12-05 10:27:57 +0330

    urls.py | add url create order and detail order | rezaamin

--- shop_django/project/orders/urls.py
+++ shop_django/project/orders/urls.py
@@ -7,5 +7,7 @@ urlpatterns=[
     path('cart/' , views.CartView.as_view() , name='cart'),
     path('card/add/<int:product_id>/' , views.CartAddView.as_view() , name='cart_add'),
     path('cart/remove/<int:product_id>/' , views.CartRemoveView.as_view() , name='cart_remove'),
+    path('cart/create/' , views.OrderCreateView.as_view() , name='order_create'),
+    path('detail/<int:order_id>/' , views.OrderDetailView.as_view() , name='orders_detail'),
 
 ]
