44e7e8e9760e05b55884fea97358dc7a1fed0009
mrisis <rezaamin8889@gmail.com>
2022-12-05 09:32:21 +0330

    views.py | create view CartRemoveView | rezaamin

--- shop_django/project/orders/views.py
+++ shop_django/project/orders/views.py
@@ -19,4 +19,31 @@ class CartAddView(View):
             cart.add(product,cd['quantity'])
         return redirect('orders:cart')
 
-    
+class CartRemoveView(View):
+    def get(self,request,product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product , id=product_id)
+        cart.remove(product)
+        return redirect('orders:cart')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
