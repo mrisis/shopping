ece611d24232b83e14edc842723ad8cf55322d2d
mrisis <rezaamin8889@gmail.com>
2022-12-05 10:14:36 +0330

    urls.py | fix bug | rezaamin

--- shop_django/project/orders/urls.py
+++ shop_django/project/orders/urls.py
@@ -6,6 +6,6 @@ app_name='orders'
 urlpatterns=[
     path('cart/' , views.CartView.as_view() , name='cart'),
     path('card/add/<int:product_id>/' , views.CartAddView.as_view() , name='cart_add'),
-    apth('cart/remove/<int:product_id>/' , views.CartRemoveView.as_view() , name='cart_remove'),
+    path('cart/remove/<int:product_id>/' , views.CartRemoveView.as_view() , name='cart_remove'),
 
 ]
