04be9b5c2f065d9bf7cfa22a42777a3d8b079a8f
mrisis <rezaamin8889@gmail.com>
2022-12-05 10:44:39 +0330

    views.py | create OrderCreateView | rezaamin

--- shop_django/project/orders/views.py
+++ shop_django/project/orders/views.py
@@ -34,6 +34,16 @@ class OrderDetailView(LoginRequiredMixin,View):
         order = get_object_or_404(Order , id=order_id)
         return render(request,'orders/order.html' , {'order':order})
 
+class OrderCreateView(LoginRequiredMixin,View):
+    def get(self,request):
+        cart = Cart(request)
+        order = Order.objects.create(user=request.user)
+
+        for item in cart :
+            OrderItem.objects.create(order=order, product=item['product'], price=item['price'] , quantity=item['quantity'])
+            cart.clear()
+
+        return redirect('orders:orders_detail',order.id)
 
 
 
