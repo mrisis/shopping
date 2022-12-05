7ff4545166ee1da18594716cd326640916ca6f73
mrisis <rezaamin8889@gmail.com>
2022-12-05 10:15:20 +0330

    admin.py | add models to admin panel | rezaamin

--- shop_django/project/orders/admin.py
+++ shop_django/project/orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . models import OrderItem , Order
 
-# Register your models here.
+
+class OrderItemInLine(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ('product',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'user' , 'created' , 'updated' , 'paid']
+    list_filter = ('paid',)
+    inlines = (OrderItemInLine,)
+
+
