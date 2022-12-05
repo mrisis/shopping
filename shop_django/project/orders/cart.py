2204b4d5a6b16a23f2628c9734e99f133b130718
mrisis <rezaamin8889@gmail.com>
2022-12-05 09:31:35 +0330

    cart.py | create function for remove | rezaamin

--- shop_django/project/orders/cart.py
+++ shop_django/project/orders/cart.py
@@ -31,3 +31,10 @@ class Cart :
 
     def get_total_price(self):
         return sum(int(item['price']) * item['quantity'] for item in self.cart.values())
+
+
+    def remove(self,product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
