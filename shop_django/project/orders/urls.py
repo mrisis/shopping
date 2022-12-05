33221818e6721f835ee67a4f7b80bed308a4db1b
mrisis <rezaamin8889@gmail.com>
2022-12-05 09:31:57 +0330

    urls.py | add url remove item from cart | rezaamin

--- shop_django/project/orders/urls.py
+++ shop_django/project/orders/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name='orders'
 urlpatterns=[
     path('cart/' , views.CartView.as_view() , name='cart'),
-    path('card/add/<int:product_id>/' , views.CartAddView.as_view() , name='cart_add')
+    path('card/add/<int:product_id>/' , views.CartAddView.as_view() , name='cart_add'),
+    apth('cart/remove/<int:product_id>/' , views.CartRemoveView.as_view() , name='cart_remove'),
 
 ]
