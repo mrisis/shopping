d8a5a815b609e28ffb502eb4a436e6061171f418
mrisis <rezaamin8889@gmail.com>
2022-12-05 09:08:18 +0330

    views.py | compelte view for add to cart | rezaamin

--- shop_django/project/orders/views.py
+++ shop_django/project/orders/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404 , redirect
 from django.views import View
-
-
+from . cart import Cart
+from home.models import Product
+from . forms import CardAddForm
 class CartView(View):
     def get(self,request):
-        return render(request,'orders/cart.html')
+        cart=Cart(request)
+        return render(request,'orders/cart.html',{'cart':cart})
 
 
 class CartAddView(View):
-    def post(self,reuest,product_id):
-        pass
+    def post(self,request,product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product,id=product_id)
+        form = CardAddForm(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+            cart.add(product,cd['quantity'])
+        return redirect('orders:cart')
+
     
