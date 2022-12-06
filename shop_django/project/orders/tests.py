747c340a617f3938ad18e2e982e82d6c4b23a066
mrisis <rezaamin8889@gmail.com>
2022-12-06 20:02:40 +0330

    tests.py | orders | write tests for model Order and Coupon | rezaamin

--- shop_django/project/orders/tests.py
+++ shop_django/project/orders/tests.py
@@ -1,3 +1,77 @@
 from django.test import TestCase
+from .models import Order , OrderItem , Coupon
+from accounts.models import User
+from home.models import Product
 
-# Create your tests here.
+class TestOrderModel(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email='rezaamin8889@gmail.com',
+            phone_number='09123456789',
+            full_name='reza amin',
+            password='123456789'
+
+        )
+        self.order = Order.objects.create(
+            user=self.user,
+            paid=True,
+            discount=10
+        )
+
+    def test_order_creation(self):
+        self.assertEqual(self.order.user, self.user)
+        self.assertEqual(self.order.paid, True)
+        self.assertEqual(self.order.discount, 10)
+        self.assertEqual(self.order.get_total_price(), 0)
+        self.assertEqual(str(self.order), f'{self.user} - {self.order.id}')
+
+    def test_order_str(self):
+        self.assertEqual(str(self.order), f'{self.user} - {self.order.id}')
+
+    def test_order_get_total_price(self):
+        self.assertEqual(self.order.get_total_price(), 0)
+
+
+# class TestOrderItemModel(TestCase):
+#     def setUp(self):
+#         self.user = User.objects.create_user(
+#             email='rezaamin8889@gmail.com',
+#             phone_number='09123456789',
+#             full_name='reza amin',
+#             password='123456789'
+#
+#         )
+#         self.order = Order.objects.create(
+#             user=self.user,
+#             paid=True,
+#             discount=10
+#         )
+#         self.product = Product.objects.create(
+
+
+
+class TestCouponModel(TestCase):
+    def setUp(self):
+        self.coupon = Coupon.objects.create(
+            code='test',
+            valid_form='2020-12-12',
+            valid_to='2020-12-12',
+            discount=10,
+            active=True
+        )
+
+    def test_coupon_creation(self):
+        self.assertEqual(self.coupon.code, 'test')
+        self.assertEqual(self.coupon.valid_form, '2020-12-12')
+        self.assertEqual(self.coupon.valid_to, '2020-12-12')
+        self.assertEqual(self.coupon.discount, 10)
+        self.assertEqual(self.coupon.active, True)
+        self.assertEqual(str(self.coupon), self.coupon.code)
+
+    def test_coupon_str(self):
+        self.assertEqual(str(self.coupon), self.coupon.code)
+
+
+
+
+
