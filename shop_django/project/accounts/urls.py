25b1229db8b291a78f3b74a9a6a67fe5e503c043
mrisis <rezaamin8889@gmail.com>
2022-12-07 09:33:30 +0330

    urls.py | accounts | add url UserPasswordReseConfirm | rezaamin

--- shop_django/project/accounts/urls.py
+++ shop_django/project/accounts/urls.py
@@ -10,5 +10,7 @@ urlpatterns=[
     path('profile/<int:user_id>/' , views.UserProfileView.as_view() , name='user_profile'),
     path('rest/' , views.UserPasswordResetView.as_view() , name='rest_password'),
     path('reset/done/', views.UserPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('confirm/<uidb64>/<token>/', views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+
 
 ]
