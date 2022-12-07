3ce22a6fed2cf5aed44b20a36832ab4285493bcb
mrisis <rezaamin8889@gmail.com>
2022-12-07 09:33:17 +0330

    views.py | accounts | create UserPasswordReseConfirm | rezaamin

--- shop_django/project/accounts/views.py
+++ shop_django/project/accounts/views.py
@@ -99,7 +99,9 @@ class UserPasswordResetView(auth_views.PasswordResetView):
 
 class UserPasswordResetDoneView(auth_views.PasswordResetDoneView):
     template_name = 'accounts/password_reset_done.html'
-
+class UserPasswordResetConfirmView(auth_views.PasswordResetConfirmView):
+    template_name = 'accounts/password_reset_confirm.html'
+    success_url = reverse_lazy('accounts:password_reset_complete')
 
 
 
