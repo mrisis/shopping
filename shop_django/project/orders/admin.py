b3fde5ba97d69af781b9d19695ad7afd6306fdb3
mrisis <rezaamin8889@gmail.com>
2022-12-09 20:49:46 +0330

    admin.py| orders | add BaseAdmin in OrderAdmin | rezaamin

--- shop_django/project/orders/admin.py
+++ shop_django/project/orders/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from . models import OrderItem , Order , Coupon
+from core.admin import BaseAdmin
 
 
 class OrderItemInLine(admin.TabularInline):
@@ -7,8 +8,8 @@ class OrderItemInLine(admin.TabularInline):
     raw_id_fields = ('product',)
 
 @admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id' , 'user' , 'created' , 'updated' , 'paid']
+class OrderAdmin(BaseAdmin):
+    list_display = ['id' , 'user' , 'created' , 'updated' , 'paid','is_deleted']
     list_filter = ('paid',)
     inlines = (OrderItemInLine,)
 
