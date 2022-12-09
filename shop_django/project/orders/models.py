d0432cdb61c2a70fd4550ca04bd21fec8816e85f
mrisis <rezaamin8889@gmail.com>
2022-12-09 20:49:17 +0330

    models.py| orders | add BaseModel in Order model | rezaamin

--- shop_django/project/orders/models.py
+++ shop_django/project/orders/models.py
@@ -2,13 +2,12 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from home.models import Product
 from django.core.validators import MinValueValidator , MaxValueValidator
+from core.models import BaseModel
 
 
-class Order(models.Model):
+class Order(BaseModel):
     user = models.ForeignKey(get_user_model() , on_delete=models.CASCADE , related_name='orders')
     paid = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
     discount = models.IntegerField(blank=True , null=True , default=None)
 
 
