7d409aedc55dbd70fb13e26af59df81720644302
mrisis <rezaamin8889@gmail.com>
2022-12-09 19:58:56 +0330

    admin.py| home | fixing bug | rezaamin

--- shop_django/project/home/admin.py
+++ shop_django/project/home/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
-from . models import Category,Product
 
-admin.site.register(Category)
+from accounts import models
+from . models import Category,Product
+from core.admin import BaseAdmin
+from django.contrib import admin
+
+
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(BaseAdmin):
+    list_display = ('name','price','availabel','is_active','is_deleted')
     row_id_fields=('category',)
+    prepopulated_fields = {'slug':('name',)}
 
 
 
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','slug','is_sub','sub_category')
+    prepopulated_fields = {'slug':('name',)}
