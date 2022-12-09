7ecee9ddd3bc328d8d226b707cb442d8bf24721f
mrisis <rezaamin8889@gmail.com>
2022-12-09 19:23:01 +0330

    admin.py| core | create model BaseModel | rezaamin

--- shop_django/project/core/admin.py
+++ shop_django/project/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.utils import timezone
 
-# Register your models here.
+
+class BaseAdmin(admin.ModelAdmin):
+    def logical_deleter(self, request, queryset):
+        queryset.update(deleted_at=timezone.now())
+        queryset.update(is_deleted=True)
+        queryset.update(is_active=False)
+
+    def deactivate(self, request, queryset):
+        queryset.update(is_active=False)
+
+    def activate(self, request, queryset):
+        queryset.update(is_active=True)
+
+    actions = ['logical_deleter', 'deactivate', 'activate']
+
+
