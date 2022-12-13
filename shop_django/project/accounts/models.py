9c240d6ee56354e6733dd556a14f61ae5744e4eb
mrisis <rezaamin8889@gmail.com>
2022-12-13 15:50:15 +0330

    models.py | account |  edit model User adding image field  | rezaamin

--- shop_django/project/accounts/models.py
+++ shop_django/project/accounts/models.py
@@ -6,6 +6,7 @@ class User(AbstractBaseUser,PermissionsMixin):
     email = models.EmailField(max_length=50,unique=True)
     phone_number = models.CharField(max_length=11,unique=True)
     full_name = models.CharField(max_length=50)
+    image = models.ImageField(null=True,blank=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     USERNAME_FIELD = 'phone_number'
