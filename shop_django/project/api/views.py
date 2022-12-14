62f8784fabf2636638113bd020c7c0ec7fb03e97
mrisis <rezaamin8889@gmail.com>
2022-12-14 14:58:15 +0330

    views.py | api |  create UserListApiView  | rezaamin

--- shop_django/project/api/views.py
+++ shop_django/project/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from accounts.models import User
+from .serializers import UserListSerializer
 
-# Create your views here.
+
+class UserListAPIView(APIView):
+    def get(self,request):
+        users = User.objects.filter(is_active=True)
+        ser_data = UserListSerializer(instance=users , many=True)
+        return Response(data = ser_data.data)
