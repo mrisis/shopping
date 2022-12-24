89ef9e49cf9bf1e44ae0b90cebd5688a3e7361d4
mrisis <rezaamin8889@gmail.com>
2022-12-24 10:50:33 +0330

    models.py | home |  adding trranslation  | rezaamin

--- shop_django/project/home/models.py
+++ shop_django/project/home/models.py
@@ -2,16 +2,18 @@ from django.db import models
 from django.urls import reverse
 from core.models import BaseModel
 from accounts.models import User
+from django.utils.translation import gettext_lazy as _
 
 class Category(models.Model):
-    sub_category= models.ForeignKey('self',on_delete=models.CASCADE , related_name='scategory',null=True,blank=True)
-    is_sub = models.BooleanField(default=False)
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100 , unique=True)
+    sub_category= models.ForeignKey('self',on_delete=models.CASCADE , related_name='scategory',null=True,blank=True,verbose_name=_('Sub Category'))
+    is_sub = models.BooleanField(default=False,verbose_name=_('Is Sub Category'))
+    name = models.CharField(max_length=100,verbose_name=_('Name'))
+    slug = models.SlugField(max_length=100 , unique=True,verbose_name=_('Slug'))
 
     class Meta:
         ordering = ('name' , )
-        verbose_name_plural = 'categories'
+        verbose_name = _('Category')
+        verbose_name_plural = _('categories')
 
     def __str__(self):
         return self.name
@@ -22,18 +24,21 @@ class Category(models.Model):
 
 
 class Product(BaseModel):
-    category = models.ManyToManyField(Category , related_name='products')
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100,unique=True)
-    image = models.ImageField()
-    description = models.TextField()
-    price = models.IntegerField()
-    availabel = models.BooleanField(default=True)
+    category = models.ManyToManyField(Category , related_name='products',verbose_name=_('Category'))
+    name = models.CharField(max_length=100,verbose_name=_('Name'))
+    slug = models.SlugField(max_length=100,unique=True,verbose_name=_('Slug'))
+    image = models.ImageField(verbose_name=_('Image'))
+    description = models.TextField(verbose_name=_('Description'))
+    price = models.IntegerField(verbose_name=_('Price'))
+    availabel = models.BooleanField(default=True,verbose_name=_('Availabel'))
 
 
 
     class Meta:
         ordering = ('name',)
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
+
 
     def __str__(self):
         return self.name
@@ -43,12 +48,15 @@ class Product(BaseModel):
 
 
 class Comment(BaseModel):
-    user = models.ForeignKey(User,on_delete=models.CASCADE , related_name='comments')
-    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='products_comments')
-    body = models.TextField(max_length=300)
-    replay = models.ForeignKey('Comment',on_delete=models.CASCADE ,related_name='replay_comments',blank=True , null=True)
-    is_replay = models.BooleanField(default=False)
+    user = models.ForeignKey(User,on_delete=models.CASCADE , related_name='comments',verbose_name=_('User'))
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='products_comments',verbose_name=_('Product'))
+    body = models.TextField(max_length=300,verbose_name=_('Body'))
+    replay = models.ForeignKey('Comment',on_delete=models.CASCADE ,related_name='replay_comments',blank=True , null=True,verbose_name=_('Replay'))
+    is_replay = models.BooleanField(default=False,verbose_name=_('Is Replay'))
 
+    class Meta:
+        verbose_name = _('Comment')
+        verbose_name_plural = _('Comments')
 
     def __str__(self):
         return f'{self.user} - {self.body[:50]}'
