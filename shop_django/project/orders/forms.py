690e6bb2d7b09c4eacf55ad46ae59eafb1b19da3
mrisis <rezaamin8889@gmail.com>
2022-12-24 10:49:14 +0330

    forms.py | orders |  adding trranslation  | rezaamin

--- shop_django/project/orders/forms.py
+++ shop_django/project/orders/forms.py
@@ -1,9 +1,10 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 class CardAddForm(forms.Form):
     quantity=forms.IntegerField(label='',min_value=1 , max_value=10,
-                                widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Quantity'}))
+                                widget=forms.NumberInput(attrs={'class':'form-control','placeholder':_('Quantity')}))
 
 
 class CouponApplyForm(forms.Form):
-    code = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control w-25','placeholder':'Coupon code'}))
+    code = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control w-25','placeholder':_('Coupon code')}))
