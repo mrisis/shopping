4536d508c04201d514856f4efe5f251044cbcf10
mrisis <rezaamin8889@gmail.com>
2022-12-24 10:50:21 +0330

    models.py | accounts |  adding trranslation  | rezaamin

--- shop_django/project/accounts/models.py
+++ shop_django/project/accounts/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from . managers import UserManager
+from django.utils.translation import gettext_lazy as _
 
 class User(AbstractBaseUser,PermissionsMixin):
-    email = models.EmailField(max_length=50,unique=True)
-    phone_number = models.CharField(max_length=11,unique=True)
-    full_name = models.CharField(max_length=50)
-    image = models.ImageField(null=True,blank=True)
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
+    email = models.EmailField(max_length=50,unique=True,verbose_name=_('email'))
+    phone_number = models.CharField(max_length=11,unique=True,verbose_name=_('phone_number'))
+    full_name = models.CharField(max_length=50,verbose_name=_('full_name'))
+    image = models.ImageField(null=True,blank=True,verbose_name=_('image'))
+    is_active = models.BooleanField(default=True,verbose_name=_('is_active'))
+    is_admin = models.BooleanField(default=False,verbose_name=_('is_admin'))
     USERNAME_FIELD = 'phone_number'
     REQUIRED_FIELDS= ['email' , 'full_name']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
 
     objects=UserManager()
 
@@ -32,9 +37,13 @@ class User(AbstractBaseUser,PermissionsMixin):
 #create Otp model for send sms and verify user
 
 class OtpCode(models.Model):
-    phone_number=models.CharField(max_length=11,unique=True)
-    code = models.PositiveSmallIntegerField()
-    created = models.DateTimeField(auto_now_add=True)
+    phone_number=models.CharField(max_length=11,unique=True,verbose_name=_('phone_number'))
+    code = models.PositiveSmallIntegerField(verbose_name=_('code'))
+    created = models.DateTimeField(auto_now_add=True,verbose_name=_('created'))
+
+    class Meta:
+        verbose_name = _('OtpCode')
+        verbose_name_plural = _('OtpCodes')
 
     def __str__(self):
         return f'{self.phone_number} - {self.code} - {self.created}'
