e32787d43d75869aa69c8f8c8843e9eeb3446d7e
mrisis <rezaamin8889@gmail.com>
2022-12-24 10:46:54 +0330

    admin.py | orders |  adding trranslation  | rezaamin

--- shop_django/project/orders/admin.py
+++ shop_django/project/orders/admin.py
@@ -7,10 +7,12 @@ class OrderItemInLine(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ('product',)
 
+
 @admin.register(Order)
 class OrderAdmin(BaseAdmin):
     list_display = ['id' , 'user' , 'created' , 'updated' , 'paid','is_deleted']
     list_filter = ('paid',)
+    search_fields = ['id']
     inlines = (OrderItemInLine,)
 
 
