395ea2eafb8090ee1e21faf898369910815b94ed
mrisis <rezaamin8889@gmail.com>
2022-12-24 10:53:58 +0330

    views.py | api |  create apiview for login  | rezaamin

--- shop_django/project/api/views.py
+++ shop_django/project/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from accounts.models import User
-from .serializers import UserListSerializer
+from .serializers import UserListSerializer , UserRegisterSerializer
 
 
 class UserListAPIView(APIView):
@@ -9,3 +9,17 @@ class UserListAPIView(APIView):
         users = User.objects.filter(is_active=True)
         ser_data = UserListSerializer(instance=users , many=True)
         return Response(data = ser_data.data)
+
+
+class UserRegisterApiView(APIView):
+    def post(self,request):
+        ser_data = UserRegisterSerializer(data=request.POST)
+        if ser_data.is_valid():
+            User.objects.create_user(phone_number=ser_data.validated_data['phone_number']  ,
+                                     email=ser_data.validated_data['email'] ,
+                                     full_name=ser_data.validated_data['full_name'] ,
+                                     password=ser_data.validated_data['password']
+                                     )
+            return Response(ser_data.data)
+        return Response(ser_data.errors)
+
