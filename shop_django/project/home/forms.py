701f3ae580f8ea94b8a629527c5de74356740f73
mrisis <rezaamin8889@gmail.com>
2022-12-24 10:48:50 +0330

    forms.py | home |  adding trranslation  | rezaamin

--- shop_django/project/home/forms.py
+++ shop_django/project/home/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from . models import Comment
+from django.utils.translation import gettext_lazy as _
 
 
 class CommetnCreateForm(forms.ModelForm):
@@ -10,15 +11,19 @@ class CommetnCreateForm(forms.ModelForm):
             'body':forms.Textarea(attrs={'class':'form-control w-25','placeholder':'please enter your comment'})
         }
 
+        labels = {
+            'body':_('body')
+        }
+
 
 class CommentReplayForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
         widgets={
-            'body':forms.Textarea(attrs={'class':'form-control w-25' , 'placeholder':'please enter your replay comment'})
+            'body':forms.Textarea(attrs={'class':'form-control w-25' , 'placeholder':_('please enter your replay comment')})
         }
 
 
 class ProductSearchForm(forms.Form):
-    search = forms.CharField(label='',widget=forms.TextInput(attrs={'placeholder':'Search'}))
+    search = forms.CharField(label='',widget=forms.TextInput(attrs={'placeholder':_('Search')}))
