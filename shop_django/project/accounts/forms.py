6a21df60b20e15cb468b1df50370fc39f79923da
mrisis <rezaamin8889@gmail.com>
2022-12-24 10:48:41 +0330

    forms.py | accounts |  adding trranslation  | rezaamin

--- shop_django/project/accounts/forms.py
+++ shop_django/project/accounts/forms.py
@@ -2,11 +2,11 @@ from django.core.exceptions import ValidationError
 from django import forms
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from . models import User , OtpCode
-
+from django.utils.translation import gettext_lazy as _
 
 class UserCreationForm(forms.ModelForm):
-    password1=forms.CharField(label='password',max_length=100 , widget=forms.PasswordInput)
-    password2 = forms.CharField(label='confirm password' , max_length=100 , widget=forms.PasswordInput)
+    password1=forms.CharField(label=_('password'),max_length=100 , widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_('confirm password') , max_length=100 , widget=forms.PasswordInput)
 
     class Meta:
         model = User
@@ -15,7 +15,7 @@ class UserCreationForm(forms.ModelForm):
     def clean_password2(self):
         cd=self.cleaned_data
         if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2'] :
-            raise ValidationError('password dont match')
+            raise ValidationError(_('password dont match'))
         return cd['password2']
 
     def save(self,commit=True):
@@ -34,24 +34,24 @@ class UserchangeForm(forms.ModelForm):
 
 
 class RegisterationForm(forms.Form):
-    email = forms.EmailField(label='',widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Email'}))
-    full_name=forms.CharField(label='',max_length=100,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Full Name'}))
-    phone_number = forms.CharField(label='',max_length=11,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Phone Number'}))
-    password = forms.CharField(label='',widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Password'}))
+    email = forms.EmailField(label='',widget=forms.TextInput(attrs={'class':'form-control','placeholder':_('Email')}))
+    full_name=forms.CharField(label='',max_length=100,widget=forms.TextInput(attrs={'class':'form-control','placeholder':_('Full Name')}))
+    phone_number = forms.CharField(label='',max_length=11,widget=forms.TextInput(attrs={'class':'form-control','placeholder':_('Phone Number')}))
+    password = forms.CharField(label='',widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':_('Password')}))
 
 
     def clean_email(self):
         email = self.cleaned_data['email']
         user = User.objects.filter(email=email)
         if user :
-            raise ValidationError('this email already exist')
+            raise ValidationError(_('this email already exist'))
         return email
 
     def clean_phone_number(self):
         phone_number = self.cleaned_data['phone_number']
         user = User.objects.filter(phone_number=phone_number)
         if user :
-            raise ValidationError('this phone number already exist')
+            raise ValidationError(_('this phone number already exist'))
         OtpCode.objects.filter(phone_number=phone_number).delete()
         return phone_number
 
@@ -60,12 +60,12 @@ class RegisterationForm(forms.Form):
 
 
 class VerifyCodeForm(forms.Form):
-    code = forms.IntegerField(widget=forms.PasswordInput(attrs={'class':'form-control','palceholder':'code'}))
+    code = forms.IntegerField(widget=forms.PasswordInput(attrs={'class':'form-control','palceholder':_('code')}))
 
 
 class UserLoginForm(forms.Form):
-    phone_number = forms.CharField(label='' , widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Phone Number OR Email'}))
-    password = forms.CharField(label='',max_length=100,widget=forms.PasswordInput(attrs={'class':'form-control' , 'placeholder':'password'}))
+    phone_number = forms.CharField(label='' , widget=forms.TextInput(attrs={'class':'form-control','placeholder':_('Phone Number OR Email')}))
+    password = forms.CharField(label='',max_length=100,widget=forms.PasswordInput(attrs={'class':'form-control' , 'placeholder':_('password')}))
 
 
 class EditProfileForm(forms.ModelForm):
@@ -73,10 +73,17 @@ class EditProfileForm(forms.ModelForm):
         model = User
         fields=('email','phone_number' , 'full_name','image')
         widgets = {
-            'email':forms.TextInput(attrs={'class':'form-control','placeholder':'Email'}),
-            'phone_number':forms.TextInput(attrs={'class':'form-control','placeholder':'Phone Number'}),
-            'full_name':forms.TextInput(attrs={'class':'form-control','placeholder':'Full Name'}),
-            'image':forms.FileInput(attrs={'class':'form-control','placeholder':'Image'}),
+            'email':forms.TextInput(attrs={'class':'form-control','placeholder':_('Email')}),
+            'phone_number':forms.TextInput(attrs={'class':'form-control','placeholder':_('Phone Number')}),
+            'full_name':forms.TextInput(attrs={'class':'form-control','placeholder':_('Full Name')}),
+            'image':forms.FileInput(attrs={'class':'form-control','placeholder':_('Image')}),
+        }
+
+        labels = {
+            'email':_('Email'),
+            'phone_number':_('Phone Number'),
+            'full_name':_('Full Name'),
+            'image':_('Image'),
         }
 
 
