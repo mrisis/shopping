c86064044a1da847551b9b0ae52bab76852f3655
mrisis <rezaamin8889@gmail.com>
2022-12-25 15:23:23 +0330

    urls.py | api |  create serializer for profile edit   | rezaamin

--- shop_django/project/api/serializers.py
+++ shop_django/project/api/serializers.py
@@ -1,32 +1,9 @@
 from rest_framework import serializers
 from accounts.models import User
 
-
-
-class UserListSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    email = serializers.EmailField()
-    phone_number = serializers.CharField(max_length=100)
-    full_name = serializers.CharField(max_length=100)
-    is_active = serializers.BooleanField(default=True)
-    is_admin = serializers.BooleanField(default=False)
-
-
-class UserRegisterSerializer(serializers.Serializer):
-    email = serializers.EmailField(required=True)
-    full_name = serializers.CharField(max_length=100,required=True)
-    phone_number = serializers.CharField(max_length=11,required=True)
-    password = serializers.CharField(max_length=50,write_only=True,required=True)
-
-    def validate_email (self,value):
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("Email already exists")
-        return value
-
-    def validate_phon_number(self,value):
-        if User.objects.filter(phone_number=value).exists():
-            raise serializers.ValidationError("Phone Number already exists")
-
-
+class UserEditProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','full_name','email','phone_number']
 
 
