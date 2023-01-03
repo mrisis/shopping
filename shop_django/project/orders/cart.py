30f99bc4d2f08d81b2a776b6eb1a7ca430deebac
mrisis <rezaamin8889@gmail.com>
2023-01-03 12:42:56 +0330

    | cart.py | adding method decrease |

--- shop_django/project/orders/cart.py
+++ shop_django/project/orders/cart.py
@@ -25,11 +25,25 @@ class Cart :
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values() )
 
-    def add(self,product,quantity):
+    def add(self,product,quantity=1):
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity':0 , 'price':str(product.price)}
-            self.cart[product_id]['quantity'] += quantity
+
+        self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def decrease(self,product,quantity=1):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            self.cart[product_id]['quantity'] -= quantity
+            if self.cart[product_id]['quantity'] <= 0:
+                self.remove(product)
+            self.save()
+
+
+
+
 
     def save(self):
         self.session.modified = True
