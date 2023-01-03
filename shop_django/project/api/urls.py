66e49f59588ac1eacf2a2401806caa0fef760b0b
mrisis <rezaamin8889@gmail.com>
2023-01-03 18:00:26 +0330

    | urls.py  | add urls api comments |

--- shop_django/project/api/urls.py
+++ shop_django/project/api/urls.py
@@ -9,5 +9,11 @@ urlpatterns = [
     path('product/list/' , views.ProductListApiView.as_view() , name='product_list_api'),
     path('category/list/' , views.CategoryListApiView.as_view() , name='category_list_api'),
     path('product/detail/<str:prdoct_slug>/' , views.ProductDetailApiView.as_view() , name='product_detail_api'),
+    path('bucket/' , views.BucketListApiView.as_view() , name='bucket_api'),
+    path('bucket/delete/' , views.BucketDeleteApiView.as_view() , name='bucket_delete_api'),
+    path('bucket/download/' , views.BucketDownloadApiView.as_view() , name='bucket_download_api'),
+    path('comment/create/',views.CommentCreateApiView.as_view() , name='comment_create_api'),
+    path('reply/create/<int:comment_id>/',views.CommentReplyApiView.as_view() , name='reply_create_api'),
+    path('comment/list/<slug:product_slug>/',views.CommentListApiView.as_view() , name='comment_list_api'),
 
 ]
