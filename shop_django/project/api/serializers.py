a637dd46a043df3d40d7bf64f6bcf66c4df6cb8f
mrisis <rezaamin8889@gmail.com>
2023-01-03 12:44:02 +0330

    | serializers.py | create serializer for apis |

--- shop_django/project/api/serializers.py
+++ shop_django/project/api/serializers.py
@@ -1,9 +1,34 @@
 from rest_framework import serializers
 from accounts.models import User
+from home.models import Product ,Category
 
-class UserEditProfileSerializer(serializers.ModelSerializer):
+class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id','full_name','email','phone_number']
+        fields = ('id','full_name','email','phone_number','image','is_active','is_admin','is_superuser')
 
 
+class ProfileEditSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('full_name','email','phone_number')
+
+
+# home app serializers
+
+class ProductListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('id','name','slug','category','image','description','price','availabel')
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id','name','slug','is_sub','sub_category')
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('name' , 'image' , 'description' , 'price')
+
+# orders app serializers
