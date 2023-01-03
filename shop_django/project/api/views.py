80e5dc2e6c83fb9840cd4023bd3a1d63b9dd4924
mrisis <rezaamin8889@gmail.com>
2023-01-03 18:00:52 +0330

    | views.py  | create apiview for comment create |

--- shop_django/project/api/views.py
+++ shop_django/project/api/views.py
@@ -1,10 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import ProfileSerializer , ProfileEditSerializer , ProductListSerializer , CategoryListSerializer,ProductDetailSerializer
+from .serializers import ProfileSerializer , ProfileEditSerializer , ProductListSerializer ,\
+    CategoryListSerializer,ProductDetailSerializer , CommentSerializer , CommentReplySerializer
 from accounts.models import User
 from rest_framework import generics
-from home.models import Product , Category
+from home.models import Product , Category , Comment
+from bucket import bucket
 
 
 
@@ -44,5 +46,65 @@ class ProductDetailApiView(APIView):
         product = Product.objects.get(slug=prdoct_slug)
         ser_data = ProductDetailSerializer(instance=product)
         return Response(ser_data.data , status = status.HTTP_200_OK)
+
+class BucketListApiView(APIView):
+    def get(self,request):
+        bucket_data = bucket.get_objects()
+        return Response(bucket_data['Contents'] , status = status.HTTP_200_OK)
+
+
+class BucketDeleteApiView(APIView):
+    def delete(self,request):
+        bucket.delete_object()
+        return Response( data={'message':'deleted bucket object'},status = status.HTTP_200_OK)
+
+class BucketDownloadApiView(APIView):
+    def get(self,request):
+        bucket.download_object()
+        return Response(data={'message':'downloaded bucket object'} , status=status.HTTP_200_OK)
+
+
+class CommentCreateApiView(APIView):
+    def post(self,request):
+        user = request.user
+        product = Product.objects.get(id=request.data['product'])
+        ser_data = CommentSerializer(data=request.data)
+        if ser_data.is_valid():
+            ser_data.save(user=user,product=product)
+            return Response(ser_data.data , status = status.HTTP_200_OK)
+        return Response(ser_data.errors , status = status.HTTP_400_BAD_REQUEST)
+
+
+
+class CommentReplyApiView(APIView):
+    def post(self,request , comment_id):
+        user = request.user
+        comment = Comment.objects.get(id=comment_id)
+        ser_data = CommentReplySerializer(data = request.data)
+        ser_data.is_valid(raise_exception=True)
+        ser_data.save(user=user,product=comment.product,is_replay=True,replay=comment)
+        return Response(ser_data.data , status = status.HTTP_200_OK)
+
+
+
+
+
+
+class CommentListApiView(APIView):
+    def get(self,request,product_slug):
+        product = Product.objects.get(slug=product_slug)
+        comments = product.products_comments.all()
+        ser_data = CommentSerializer(comments , many = True)
+        return Response(ser_data.data , status = status.HTTP_200_OK)
+
+
+
+
+
+
+
+
+
+
 
 # orders app api views
