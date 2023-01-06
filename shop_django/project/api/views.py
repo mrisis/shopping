eca5f7372552697bcb6835ed5224b9803e57e156
mrisis <rezaamin8889@gmail.com>
2023-01-06 10:01:55 +0330

    | views.py  | create order detail api |

--- shop_django/project/api/views.py
+++ shop_django/project/api/views.py
@@ -24,6 +24,12 @@ class ProfileApiView(APIView):
 
 
 class ProfileEditApiView(APIView):
+    '''
+        method put:
+          for edit and update user profile
+
+
+    '''
     def put(self,request,user_id):
         user = User.objects.get(id=user_id)
         ser_data = ProfileEditSerializer(instance=user , data=request.data,partial=True)
@@ -36,38 +42,75 @@ class ProfileEditApiView(APIView):
 # home app api views
 
 class ProductListApiView(generics.ListAPIView):
+    '''
+        method get:
+            for get all products
+    '''
     queryset = Product.objects.filter(availabel=True)
     serializer_class = ProductListSerializer
 
 class CategoryListApiView(generics.ListAPIView):
+
+    '''
+        method get:
+            for get all categories
+    '''
+
+
+
     queryset = Category.objects.filter(is_sub=False)
     serializer_class = CategoryListSerializer
 
 
 class ProductDetailApiView(APIView):
+
+    '''
+        method get:
+            for get product detail
+    '''
     def get(self,request,prdoct_slug):
         product = Product.objects.get(slug=prdoct_slug)
         ser_data = ProductDetailSerializer(instance=product)
         return Response(ser_data.data , status = status.HTTP_200_OK)
 
 class BucketListApiView(APIView):
+
+    '''
+        method get:
+            for get all bucket objects in arvan cloud
+    '''
     def get(self,request):
         bucket_data = bucket.get_objects()
         return Response(bucket_data['Contents'] , status = status.HTTP_200_OK)
 
 
 class BucketDeleteApiView(APIView):
+
+    '''
+        method delete:
+            for delete bucket object in arvan cloud
+    '''
     def delete(self,request):
         bucket.delete_object()
         return Response( data={'message':'deleted bucket object'},status = status.HTTP_200_OK)
 
 class BucketDownloadApiView(APIView):
+
+    '''
+        method get:
+            for download bucket object in arvan cloud
+    '''
     def get(self,request):
         bucket.download_object()
         return Response(data={'message':'downloaded bucket object'} , status=status.HTTP_200_OK)
 
 
 class CommentCreateApiView(APIView):
+
+    '''
+        method post:
+            for create comment
+    '''
     def post(self,request):
         user = request.user
         product = Product.objects.get(id=request.data['product'])
@@ -80,6 +123,11 @@ class CommentCreateApiView(APIView):
 
 
 class CommentReplyApiView(APIView):
+
+    '''
+        method post:
+            for create comment reply
+    '''
     def post(self,request , comment_id):
         user = request.user
         comment = Comment.objects.get(id=comment_id)
@@ -94,6 +142,11 @@ class CommentReplyApiView(APIView):
 
 
 class CommentListApiView(APIView):
+
+    '''
+        method get:
+            for get all comments of product and reply comments
+    '''
     def get(self,request,product_slug):
         product = Product.objects.get(slug=product_slug)
         comments = product.products_comments.all()
@@ -103,12 +156,25 @@ class CommentListApiView(APIView):
 # orders app api views
 
 class CartApiView(APIView):
+
+    '''
+        method get:
+            for get all cart items
+    '''
+
+
     def get(self,request):
         cart = Cart(request)
         return Response(cart.cart , status = status.HTTP_200_OK)
 
 
 class CartAddAPiView(APIView):
+
+    '''
+        method post:
+            for add product item to cart
+    '''
+
     def post(self,request,product_id):
         cart = Cart(request)
         product = Product.objects.get(id=product_id)
@@ -120,6 +186,11 @@ class CartAddAPiView(APIView):
 
 
 class CartRemoveApiView(APIView):
+
+    '''
+        method delete:
+            for remove product item form cart
+    '''
     def delete(self,request,product_id):
         cart = Cart(request)
         product = Product.objects.get(id=product_id)
@@ -129,6 +200,11 @@ class CartRemoveApiView(APIView):
 
 
 class OrderCreateApiView(APIView):
+
+    '''
+        method post:
+            for create order and save order in database
+    '''
     def post(self,request):
         order = Order.objects.create(user=request.user)
         cart = Cart(request)
@@ -141,6 +217,17 @@ class OrderCreateApiView(APIView):
         return Response(result , status = status.HTTP_200_OK)
 
 
+
+class OrderDetailApiView(APIView):
+
+    '''
+        method get:
+            for get order detail and show order items
+    '''
+    def get(self,request,order_id):
+        order = Order.objects.get(id=order_id)
+        ser_data = OrderSerializer(instance=order)
+        return Response(ser_data.data , status = status.HTTP_200_OK)
 
 
 
